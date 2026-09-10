d74de466441709797a3434117cf763c63a93334b
NoxAnimusVicta <NoxAnimusVicta@gmail.com>
2026-09-10 21:18:11 +1000

    Rebuild Cycle 5 Viorla turn website

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='dessica-b8320a006e32';
+const CACHE='dessica-afc4e36495d1';
 const ASSETS=['./','./index.html','./campaign.json','./Dessica_Campaign.md','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('dessica-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
