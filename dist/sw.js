f854ef2f0dbfc04e516cde5b427acf7339c8d869
NoxAnimusVicta <NoxAnimusVicta@gmail.com>
2026-09-10 16:00:39 +1000

    Rebuild site with Cycle Records narration

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='dessica-09265659c66c';
+const CACHE='dessica-8099d4b6393c';
 const ASSETS=['./','./index.html','./campaign.json','./Dessica_Campaign.md','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('dessica-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
